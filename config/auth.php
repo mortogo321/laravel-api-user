38bb6d249f8ee8f8d7b65a0d969b46528cd58664
Mor <mortogo321@gmail.com>
2025-05-12 14:36:09 +0700

    add guard api

--- config/auth.php
+++ config/auth.php
@@ -42,6 +42,10 @@ return [
             'driver' => 'session',
             'provider' => 'users',
         ],
+        'api' => [
+            'driver' => 'sanctum',
+            'provider' => 'users',
+        ],
     ],
 
     /*
